f9aa91dda8a8848db466e98bca0fec655777c4d1
Muhalif7123 <muhalif7123@gmail.com>
2024-12-15 22:48:00 +0800

    initial comit

---
+++ routes.js
@@ -0,0 +1,70 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return JSON.stringify({
+            message: 'Homepage'
+            });
+        }
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses menggunakan method tersebut';
+        },
+    },
+
+
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses menggunakan method tersebut';
+        },
+    },
+
+
+    {
+        method: 'GET',
+        path: '/hello/{username?}',
+        handler: (request, h) => {
+            const { username = 'stranger' } = request.params;
+            const { lang } = request.query;
+            if (lang === 'ID') {
+                return { message: `Hai ${username}`};
+            }
+            return { message: `Hello ${username}`};
+        }
+    },
+
+
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}, your password ${password}`;
+        },
+    },
+
+
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+
+];
+
+module.exports = routes;
